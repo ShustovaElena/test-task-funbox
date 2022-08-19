6bc89c0931727f6bc5d31103230db4562227784c
Elena Shustova <alenyf-ka@mail.ru>
2022-08-19 10:13:39 +0300

    feat: add layout of package

---
+++ test-task-funbox/src/components/Package/index.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+export const Package = () => {
+    return (
+        <div className="package">
+            <div className='package-text'>
+                <p className="package-header">Сказочное заморское явство</p>
+                <h2 className="package-name">Нямушка</h2>
+                <h3 className="package-type">с фуа-гра</h3>
+                <p>10 порций</p>
+                <p>мышь в подарок</p>
+            </div>
+        </div>
+    );
+};
